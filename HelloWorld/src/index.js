7b48337bbf7315fb831d46bc9d509dfcefbbaaf7
kazutaka nakamura <85468557+nakamurakzz@users.noreply.github.com>
2022-10-20 20:29:22 +0900

    Update index.js

--- HelloWorld/src/index.js
+++ HelloWorld/src/index.js
@@ -8,7 +8,7 @@ const OBNIZ_ID = process.env.OBNIZ_ID;
 const main = async () => {
   if (OBNIZ_ID == null) return;
 
-  const obniz = new Obniz(OBNIZ_ID ?? "");
+  const obniz = new Obniz(OBNIZ_ID);
   const connected = await obniz.connectWait({ timeout: 1000 });
 
   if (connected) {
